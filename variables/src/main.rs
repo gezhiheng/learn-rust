a20322f6aa0d684c17e82a393777247e46d10bff
Henry Ge <1981761687@qq.com>
2024-07-17 17:20:45 +0800

    finish variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,17 @@
 fn main() {
-    let x = 5;
+    let mut x = 5;
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
+
+    let y = 6;
+
+    let y = y + 1; // 变量遮蔽
+
+    {
+        let y = y * 2;
+        println!("{y}");  // 14
+    }
+
+    println!("{y}"); // 7
 }
