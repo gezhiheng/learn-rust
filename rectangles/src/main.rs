7d8b37a58947251c2582af964ebbbee58b75385c
Henry Ge <54823984+gezhiheng@users.noreply.github.com>
2024-07-23 20:51:30 +0800

    modified:   rectangles/src/main.rs

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -8,6 +8,10 @@ impl Rectangle {
     fn area(&self) -> u32 {
         &self.width * &self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width >= other.width && self.height >= other.height
+    }
 }
 
 fn main() {
@@ -21,6 +25,13 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         rect1.area()
     );
+
+    let rect2 = Rectangle {
+        width: 100,
+        height: 100,
+    };
+
+    println!("rect1 can hold rect {} ?", rect1.can_hold(&rect2));
 
     dbg!(&rect1);
 }
