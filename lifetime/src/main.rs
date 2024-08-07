71a95bde7c374880fabeddf23d3826f8b76882c1
Henry Ge <1981761687@qq.com>
2024-08-07 16:56:37 +0800

    life time

---
+++ lifetime/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let string1 = String::from("long string is long");
+    let result;
+    {
+        let string2 = String::from("xyz");
+        result = longest(string1.as_str(), string2.as_str());
+    }
+    println!("The longest string is {result}");
+}
+
+// `a 表示生命周期 两个参数和返回值引用 都是同一个生命周期
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
