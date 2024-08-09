7f62284c49553017823654e9dcf21f0f6e0c0385
Henry Ge <1981761687@qq.com>
2024-08-09 14:37:31 +0800

    refactor: mygrep

---
+++ mygrep/src/lib.rs
@@ -0,0 +1,82 @@
+use std::fs;
+use std::error::Error;
+
+pub struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            // TODO when remove return, why expected this to be ()
+            return Err("Not enough arguments...");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config{ query, file_path })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents: String = fs::read_to_string(config.file_path)?;
+
+    for line in search(&config.query, &contents) {
+        println!("line: {line}");
+    }
+    
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+
+    result
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "How public, like a frog";
+        let contents = "\
+I'm nobody! Who are you?
+Are you nobody, too?
+Then there's a pair of us - don't tell!
+They'd banish us, you know.
+How dreary to be somebody!
+How public, like a frog
+To tell your name the livelong day
+To an admiring bog!";
+
+        assert_eq!(vec!["How public, like a frog"], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "RusT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+}
--- mygrep/src/main.rs
+++ mygrep/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
+
+use mygrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,30 +11,8 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Search for {}", config.query);
-    println!("In file {}", config.file_path);
-
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file.");
-
-    println!("With Text\n{contents}");
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            // TODO why expected this to be ()
-            Err("Not enough arguments...")
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config{ query, file_path })
+    if let Err(e) = mygrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
     }
 }
