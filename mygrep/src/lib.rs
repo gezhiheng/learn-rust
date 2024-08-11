85c8b3816ed9eb803abf7fb7a82cee0fe6a4ed01
Henry Ge <54823984+gezhiheng@users.noreply.github.com>
2024-08-11 10:06:08 +0800

    handle env arguments

--- mygrep/src/lib.rs
+++ mygrep/src/lib.rs
@@ -1,9 +1,11 @@
 use std::fs;
 use std::error::Error;
+use std::env;
 
 pub struct Config {
     query: String,
     file_path: String,
+    ignore_case: bool,
 }
 
 impl Config {
@@ -15,14 +17,27 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config{ query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        if args.len() >= 4 {
+            let ignore_case_arg: bool = args[3].eq("ignorecase");
+            Ok(Config{ query, file_path, ignore_case: ignore_case_arg })
+        } else {
+            Ok(Config{ query, file_path, ignore_case })
+        }
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents: String = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let result = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+    
+    for line in result {
         println!("line: {line}");
     }
     
@@ -42,7 +57,16 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let query = query.to_lowercase();
+    let mut result = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            result.push(line)
+        }
+    }
+
+    result
 }
 
 #[cfg(test)]
@@ -50,7 +74,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "How public, like a frog";
         let contents = "\
 I'm nobody! Who are you?
