7581c2f80baeaa09c222ccc15cd27172ae5952e6
Henry Ge <1981761687@qq.com>
2024-08-14 17:31:25 +0800

    drop trait

--- learn-workspace/adder/src/main.rs
+++ learn-workspace/adder/src/main.rs
@@ -1,6 +1,16 @@
 use add_one;
+use std::collections::HashMap;
 
 fn main() {
     let num = 10;
     println!("Hello, world! {num} plus one is {}!", add_one::add_one(num));
+
+    let mut map = HashMap::<i32, i32>::new();
+
+    map.insert(1, 1);
+
+    let v = map.entry(1).or_insert(0);
+    println!("v: {v:?}");
+
+    println!("map: {map:?}");
 }
