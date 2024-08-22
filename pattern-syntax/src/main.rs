d30d0a6915df2e13584c26a2ca1cfc58e3cd628b
Henry Ge <1981761687@qq.com>
2024-08-22 13:55:48 +0800

    pattern syntax

---
+++ pattern-syntax/src/main.rs
@@ -0,0 +1,59 @@
+fn main() {
+    let x = Some(5);
+
+    match x {
+        Some(50) => println!("50"),
+        Some(y) => println!("Matched, y = {y}"),
+        _ => println!("Default case, x = {x:?}"),
+    }
+
+    println!("at the end: x = {x:?}");
+
+    let mut setting_value = Some(5);
+    let new_setting_value = Some(10);
+    match (setting_value, new_setting_value) {
+        (Some(_), Some(_)) => {
+            println!("Can't overwrite an existing customized value");
+        }
+        _ => {
+            setting_value = new_setting_value;
+        }
+    }
+    println!("setting is {setting_value:?}");
+
+    let s = Some(String::from("Hello!"));
+    if let Some(_) = s {
+        println!("found a string");
+    }
+    println!("{s:?}");
+
+
+    let num = Some(5);
+    // 匹配守卫
+    match num {
+        Some(x) if x % 2 == 0 => println!("The number {x} is even"),
+        Some(x) => println!("The number {x} is odd"),
+        None => (),
+    }
+
+    let x = 4;
+    let y = false;
+    match x {
+    4 | 5 | 6 if y => println!("yes"),
+    _ => println!("no"),
+    }
+
+    enum Message {
+        Hello { id: i32 },
+    }
+    let msg = Message::Hello { id: 5 };
+    match msg {
+        Message::Hello {
+            id: id_variable @ 3..=7,
+        } => println!("Found an id in range: {id_variable}"),
+        Message::Hello { id: 10..=12 } => {
+            println!("Found an id in another range")
+        }
+        Message::Hello { id } => println!("Found some other id: {id}"),
+    }
+}
